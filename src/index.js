5a2ad78fafa8ead54704a916c4f72d5f87a1c545
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2023-11-08 15:46:54 +0000

    Add action code

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { inspect } = require("util");
+const core = require("@actions/core");
+const j2m = require("jira2md");
+
+async function run() {
+  try {
+    const inputs = {
+      inputText: core.getInput("input-text"),
+      mode: core.getInput("mode"),
+    };
+    core.debug(`Inputs: ${inspect(inputs)}`);
+
+    switch (inputs.mode) {
+      case "jira2md": {
+        const md = j2m.to_markdown(inputs.inputText);
+        core.setOutput("output-text", md);
+        break;
+      }
+      case "md2jira": {
+        const jira = j2m.to_jira(inputs.inputText);
+        core.setOutput("output-text", jira);
+        break;
+      }
+      default:
+        core.setFailed(`Unknown mode: ${inputs.mode}`);
+    }
+  } catch (error) {
+    core.debug(inspect(error));
+    core.setFailed(error.message);
+  }
+}
+
+run();
